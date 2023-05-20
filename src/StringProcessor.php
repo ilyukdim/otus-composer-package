33048f08c84519f1bc0d88a9d4f9990a37810ffc
DiM <hy0@ya.ru>
2023-05-20 13:16:02 +0700

    add UTF-8 charset

--- src/StringProcessor.php
+++ src/StringProcessor.php
@@ -7,6 +7,20 @@ class StringProcessor
 {
     public static function getFirstSymbolUp(string $str): string
     {
-        return UCfirst($str);
+        return static::mb_ucfirst($str);
+    }
+    
+    protected static function mb_ucfirst(string $str, string $encoding = 'UTF8'): string
+    {
+        $upFisrtStr = preg_replace_callback(
+            "![a-zа-я]!u",
+            static function ($matches) use ($encoding)
+            {
+                return mb_strtoupper($matches[0], $encoding);
+            },
+            $str,
+            1
+        );
+        return $upFisrtStr;
     }
 }
